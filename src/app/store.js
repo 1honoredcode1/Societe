6aa669a76f50cd118eb3c86f02cce48f3b0d69e5
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-03-14 19:12:02 +0100

    Listing Details page has been created

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import listingReducer from "./features/listingsSlice";
 
+import chatReducer from "./features/chatSlice";
+
 export const store = configureStore({
   reducer: {
     listing: listingReducer,
+    chat: chatReducer,
   },
 });
